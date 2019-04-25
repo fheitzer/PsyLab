799bdb779e5b1a90f863d36de76b987f352e72fb
Friedrich Heitzer <floyd@83adbe1c.funky.uni-osnabrueck.de>
2019-04-25 20:58:16 +0200

    Almost done

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,22 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/wuerfel.png');
     }
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'Welcome to the Casino, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Welcome to the Casino, ' + storedName;
+}
+myButton.onclick = function() {
+  setUserName();
+}
